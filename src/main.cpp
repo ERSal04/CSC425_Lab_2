bf985daa3b9d2e4a5047fa32cde4a817dbc8304b
Grace <gracegrisham.bergquist@calbaptist.edu>
2026-02-24 10:10:42 -0800

    working challenge 1 code

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,10 @@
 #include <BLEServer.h>
 #include <BLE2902.h>
 #include <M5Unified.h>
+#include <Adafruit_seesaw.h>
 
 ///////////////////////////////////////////////////////////////
-// Variables
+// Server Variables
 ///////////////////////////////////////////////////////////////
 
 BLEServer *bleServer;
@@ -17,9 +18,42 @@ bool deviceConnected = false;
 bool previouslyConnected = false;
 int timer = 0;
 
+int last_x = 512, last_y = 512;
+
 // See the following for generating UUIDs: https://www.uuidgenerator.net/
 #define SERVICE_UUID        "4d92ed41-94fc-43a2-a9e6-e17e7f804d02"
 #define CHARACTERISTIC_UUID "99f63e2d-8c68-4206-b763-da326c24009a"
+
+///////////////////////////////////////////////////////////////
+// Controller Variables
+///////////////////////////////////////////////////////////////
+
+Adafruit_seesaw controller;
+
+int p1Posx = 160, p1Posy = 120;
+int p1Speed = 1;
+
+static const int SCREEN_W = 320;
+static const int SCREEN_H = 240;
+static const int DOT_RADIUS = 6;
+static const int JOY_CENTER = 512;
+static const int JOY_DEADZONE = 60;
+
+#define BUTTON_X 6
+#define BUTTON_Y 2
+#define BUTTON_A 5
+#define BUTTON_B 1
+#define BUTTON_SELECT 0
+#define BUTTON_START 16
+uint32_t button_mask = (1UL << BUTTON_X) | (1UL << BUTTON_Y) | (1UL << BUTTON_START) | 
+                       (1UL << BUTTON_A) | (1UL << BUTTON_B) | (1UL << BUTTON_SELECT);
+
+
+void showGameOverScreen();
+void updateP1Pos(int joyX, int joyY, uint32_t buttons);
+void drawScreen();
+///////////
+
 
 // Bluetooth callback methods
 class myServerCallbacks: public BLEServerCallbacks {
@@ -56,6 +90,28 @@ void setup() {
     drawScreenTextWithBackground("Initializing BLE...", TFT_CYAN);
     broadcastBleServer();
     drawScreenTextWithBackground("Broadcasting service/characteristic as BLE server...", TFT_BLUE);
+
+    Serial.begin(115200);
+
+    while(!Serial) {
+        delay(10);
+    }
+
+    // Use adafruit library to connect to your device
+    if (!controller.begin(0x50)) {
+    Serial.println("Could not find Controller locally..."); 
+        while (1);
+    }
+    Serial.println("Found Controller!");
+
+    controller.pinModeBulk(button_mask, INPUT_PULLUP);
+    controller.setGPIOInterrupts(button_mask, 1);
+    randomSeed(micros());
+
+    M5.Lcd.clear();
+    M5.Lcd.fillScreen(TFT_BLACK);
+
+    drawScreen();
     
 }
 
@@ -64,6 +120,7 @@ void setup() {
 ///////////////////////////////////////////////////////////////
 void loop()
 {
+    // TODO: turn this into a function
     if (deviceConnected) {
         // //  1. Update characteristic value (Which is read by client)
         // timer++;
@@ -83,7 +140,30 @@ void loop()
         timer = 0;
     }
 
-    delay(1000);
+    // TODO: hack the mainframe (add function that reads in client side x and y position)
+    // TODO: method that draws screen accordingly
+
+    M5.update();
+    // delay(100); // delay in loop to slow serial output
+
+    // Reverse x/y values to match joystick orientation
+    int x = 1023 - controller.analogRead(14);
+    int y = 1023 - controller.analogRead(15);
+    
+    if ((abs(x - last_x) > 3) || (abs(y - last_y) > 3)) {
+        Serial.print("x: "); Serial.print(x); Serial.print(", "); Serial.print("y: "); Serial.println(y);
+    }
+    last_x = x;
+    last_y = y;
+
+    uint32_t buttons = controller.digitalReadBulk(button_mask);
+
+    updateP1Pos(last_x, last_y, buttons);
+
+    drawScreen();
+
+
+    delay(16);
 }
 
 void drawScreenTextWithBackground(String text, int backgroundColor) {
@@ -115,4 +195,43 @@ void broadcastBleServer() {
     bleAdvertising->setMinPreferred(0x12);
     BLEDevice::startAdvertising();
     Serial.println("Characterisic defined...you can now connect with your phone!");
+}
+
+void drawScreen() {
+  p1Posx = constrain(p1Posx, DOT_RADIUS, SCREEN_W - DOT_RADIUS);
+  p1Posy = constrain(p1Posy, DOT_RADIUS, SCREEN_H - DOT_RADIUS);
+
+  M5.Lcd.fillScreen(TFT_BLACK);
+  M5.Lcd.fillCircle(p1Posx, p1Posy, DOT_RADIUS, TFT_BLUE);
+}
+
+
+void updateP1Pos(int joyX, int joyY, uint32_t buttons) {
+  int dx = joyX - JOY_CENTER;
+  int dy = joyY - JOY_CENTER;
+
+  if (abs(dx) > JOY_DEADZONE) {
+    p1Posx += (dx > 0) ? p1Speed : -p1Speed;
+  }
+
+  if (abs(dy) > JOY_DEADZONE) {
+    p1Posy += (dy < 0) ? p1Speed : -p1Speed;
+  }
+
+  static bool startWasPressed = false;
+  bool startPressed = !(buttons & (1UL << BUTTON_START));
+  if (startPressed && !startWasPressed) {
+    p1Speed = (p1Speed >= 5) ? 1 : p1Speed + 1;
+    Serial.printf("p1Speed = %d\n", p1Speed);
+  }
+  startWasPressed = startPressed;
+
+  static bool selectWasPressed = false;
+  bool selectPressed = !(buttons & (1UL << BUTTON_SELECT));
+  if (selectPressed && !selectWasPressed) {
+    p1Posx = random(DOT_RADIUS, SCREEN_W - DOT_RADIUS);
+    p1Posy = random(DOT_RADIUS, SCREEN_H - DOT_RADIUS);
+    Serial.printf("Warp to (%d, %d)\n", p1Posx, p1Posy);
+  }
+  selectWasPressed = selectPressed;
 }
