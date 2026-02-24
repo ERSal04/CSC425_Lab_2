7fa717d0bc1af04fc9f92e27f2d9e3ff1700391c
ERSal04 <elijahsalgado04@gmail.com>
2026-02-24 09:12:56 -0800

    Initialize Repo

---
+++ src/EGR425_Phase3_ble_client_TEMPLATE.cpp
@@ -0,0 +1,171 @@
+// // Some useful resources on BLE and ESP32:
+// //      https://github.com/nkolban/ESP32_BLE_Arduino/blob/master/examples/BLE_notify/BLE_notify.ino
+// //      https://microcontrollerslab.com/esp32-bluetooth-low-energy-ble-using-arduino-ide/
+// //      https://randomnerdtutorials.com/esp32-bluetooth-low-energy-ble-arduino-ide/
+// //      https://www.electronicshub.org/esp32-ble-tutorial/
+// #include <BLEDevice.h>
+// #include <BLE2902.h>
+// #include <M5Core2.h>
+
+// ///////////////////////////////////////////////////////////////
+// // Variables
+// ///////////////////////////////////////////////////////////////
+// static BLERemoteCharacteristic *pRemoteCharacteristic;
+// static BLEAdvertisedDevice *myDevice;
+// static boolean doConnect = false;
+// static boolean doScan = false;
+// bool deviceConnected = false;
+
+// // See the following for generating UUIDs: https://www.uuidgenerator.net/
+// static BLEUUID SERVICE_UUID("4fafc201-1fb5-459e-8fcc-c5c9c331914b"); // Dr. Dan's Service
+// static BLEUUID CHARACTERISTIC_UUID("beb5483e-36e1-4688-b7f5-ea07361b26a8"); // Dr. Dan's Characteristic
+
+// ///////////////////////////////////////////////////////////////
+// // Forward Declarations
+// ///////////////////////////////////////////////////////////////
+// void drawScreenTextWithBackground(String text, int backgroundColor);
+
+// ///////////////////////////////////////////////////////////////
+// // BLE Client Callback Methods
+// // This method is called when the server that this client is
+// // connected to NOTIFIES this client (or any client listening)
+// // that it has changed the remote characteristic
+// ///////////////////////////////////////////////////////////////
+// static void notifyCallback(BLERemoteCharacteristic *pBLERemoteCharacteristic,uint8_t *pData, size_t length, bool isNotify)
+// {
+// }
+
+// ///////////////////////////////////////////////////////////////
+// // BLE Server Callback Method
+// // These methods are called upon connection and disconnection
+// // to BLE service.
+// ///////////////////////////////////////////////////////////////
+// class MyClientCallback : public BLEClientCallbacks
+// {
+//     void onConnect(BLEClient *pclient)
+//     {
+//         deviceConnected = true;
+//         Serial.println("Device connected...");
+//     }
+
+//     void onDisconnect(BLEClient *pclient)
+//     {
+//         deviceConnected = false;
+//         Serial.println("Device disconnected...");
+//     }
+// };
+
+// ///////////////////////////////////////////////////////////////
+// // Method is called to connect to server
+// ///////////////////////////////////////////////////////////////
+// bool connectToServer()
+// {
+//     // Create the client
+
+//     // Connect to the remove BLE Server.
+
+//     // Obtain a reference to the service we are after in the remote BLE server.
+
+
+//     // Obtain a reference to the characteristic in the service of the remote BLE server.
+
+//     // Read the value of the characteristic.
+
+//     // Check if server's characteristic can notify client of changes and register to listen if so
+
+
+//     deviceConnected = true;
+// }
+
+// ///////////////////////////////////////////////////////////////
+// // Scan for BLE servers and find the first one that advertises
+// // the service we are looking for.
+// ///////////////////////////////////////////////////////////////
+// class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
+// {
+//     /**
+//      * Called for each advertising BLE server.
+//      */
+//     void onResult(BLEAdvertisedDevice advertisedDevice)
+//     {
+//         // Print device found
+
+
+//         // More debugging print
+//         //Serial.printf("\tAddress: %s\n", advertisedDevice.getAddress().toString().c_str());
+//         //Serial.printf("\tHas a ServiceUUID: %s\n", advertisedDevice.haveServiceUUID() ? "True" : "False");
+//         //for (int i = 0; i < advertisedDevice.getServiceUUIDCount(); i++) {
+//         //    Serial.printf("\t\t%s\n", advertisedDevice.getServiceUUID(i).toString().c_str());
+//         //}
+//         //Serial.printf("\tHas our service: %s\n\n", advertisedDevice.isAdvertisingService(SERVICE_UUID) ? "True" : "False");
+        
+//         // We have found a device, let us now see if it contains the service we are looking for.
+
+//     }     
+// };        
+
+// ///////////////////////////////////////////////////////////////
+// // Put your setup code here, to run once
+// ///////////////////////////////////////////////////////////////
+// void setup()
+// {
+//     // Init device
+//     M5.begin();
+//     M5.Lcd.setTextSize(3);
+
+//     BLEDevice::init("");
+
+//     // Retrieve a Scanner and set the callback we want to use to be informed when we
+//     // have detected a new device.  Specify that we want active scanning and start the
+//     // scan to run for 5 seconds.
+//     BLEScan *pBLEScan = BLEDevice::getScan();
+//     pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+//     pBLEScan->setInterval(1349);
+//     pBLEScan->setWindow(449);
+//     pBLEScan->setActiveScan(true);
+//     pBLEScan->start(5, false);
+// }
+
+// ///////////////////////////////////////////////////////////////
+// // Put your main code here, to run repeatedly
+// ///////////////////////////////////////////////////////////////
+// void loop()
+// {
+//     // If the flag "doConnect" is true then we have scanned for and found the desired
+//     // BLE Server with which we wish to connect.  Now we connect to it.  Once we are
+//     // connected we set the connected flag to be true.
+//     if (doConnect == true)
+//     {
+//         if (connectToServer())
+//             Serial.println("We are now connected to the BLE Server.");
+//         else
+//             Serial.println("We have failed to connect to the server; there is nothin more we will do.");
+//         doConnect = false;
+//     }
+
+//     // If we are connected to a peer BLE Server, update the characteristic each time we are reached
+//     // with the current time since boot.
+//     if (deviceConnected)
+//     {
+//         // Format string to send to server
+//         String newValue = "Time since boot: " + String(millis() / 1000);
+//         Serial.println("Setting new characteristic value to \"" + newValue + "\"");
+
+//         // Set the characteristic's value to be the array of bytes that is actually a string.
+//         pRemoteCharacteristic->writeValue(newValue.c_str(), newValue.length());
+//         drawScreenTextWithBackground("Wrote value to server: " + String(newValue.c_str()), TFT_YELLOW); // Give feedback on screen
+//     }
+//     else if (doScan)
+//         BLEDevice::getScan()->start(0); // this is just example to start scan after disconnect, most likely there is better way to do it in arduino
+
+//     delay(1000); // Delay a second between loops.
+// }
+
+// ///////////////////////////////////////////////////////////////
+// // Colors the background and then writes the text on top
+// ///////////////////////////////////////////////////////////////
+// void drawScreenTextWithBackground(String text, int backgroundColor) {
+//     M5.Lcd.fillScreen(backgroundColor);
+//     M5.Lcd.setCursor(0,0);
+//     M5.Lcd.println(text);
+// }
---
+++ src/EGR425_Phase3_ble_server_TEMPLATE.cpp
@@ -0,0 +1,119 @@
+// // https://randomnerdtutorials.com/esp32-bluetooth-low-energy-ble-arduino-ide/
+// // https://github.com/nkolban/ESP32_BLE_Arduino/blob/master/examples/BLE_notify/BLE_notify.ino
+// // https://www.electronicshub.org/esp32-ble-tutorial/
+// #include <M5Core2.h>
+// #include <BLEDevice.h>
+// #include <BLEServer.h>
+// #include <BLE2902.h>
+
+
+// ///////////////////////////////////////////////////////////////
+// // Variables
+// ///////////////////////////////////////////////////////////////
+
+// BLEServer *bleServer;
+// BLEService *bleService;
+// BLECharacteristic *bLeCharacteristic;
+// bool deviceConnected = false;
+// bool previouslyConnected = false;
+// int timer = 0;
+
+// // See the following for generating UUIDs: https://www.uuidgenerator.net/
+// #define SERVICE_UUID        "1a1689e5-df18-4c23-b69a-d12aa2a557ee"
+// #define CHARACTERISTIC_UUID "6df7c881-e712-4000-8bbe-755a9f961755"
+
+// // Bluetooth callback methods
+// class myServerCallbacks: public BLEServerCallbacks {
+//     void onConnect(BLEServer *pServer) {
+//         deviceConnected = true;
+//         Serial.println("Bluetooth device connected...");
+//     }
+
+//     void onDisconnect(BLEServer *pServer) {
+//         deviceConnected = false;
+//         Serial.println("Device disconnected...");
+//     }
+// };
+
+// ///////////////////////////////////////////////////////////////
+// // Forward Declarations
+// ///////////////////////////////////////////////////////////////
+// void broadcastBleServer();
+// void drawScreenTextWithBackground(String text, int backgroundColor);
+
+// ///////////////////////////////////////////////////////////////
+// // Put your setup code here, to run once
+// ///////////////////////////////////////////////////////////////
+// void setup() {
+
+//     // Init device
+//     M5.begin();
+//     M5.Lcd.setTextSize(2);
+//     Serial.println("Starting BLE...");
+    
+//     // Initialize M5 as BLE server...
+//     BLEDevice::init("ElijahSalgado's M5Core2...");
+
+//     drawScreenTextWithBackground("Initializing BLE...", TFT_CYAN);
+//     broadcastBleServer();
+//     drawScreenTextWithBackground("Broadcasting service/characteristic as BLE server...", TFT_BLUE);
+    
+// }
+
+// ///////////////////////////////////////////////////////////////
+// // Put your main code here, to run repeatedly
+// ///////////////////////////////////////////////////////////////
+// void loop()
+// {
+//     if (deviceConnected) {
+//         // //  1. Update characteristic value (Which is read by client)
+//         // timer++;
+//         // bLeCharacteristic->setValue(timer);
+//         // Serial.printf("%d written to BLE Characteristic.\n", timer);
+
+//         // // 2. Read the characteristic value as a string (Which is written from client)
+//         std::string readValue = bLeCharacteristic->getValue();
+//         Serial.printf("The new characteristic value as a string is: %s\n", readValue.c_str());
+//         String valStr = readValue.c_str();
+//         int val = valStr.toInt();
+//         Serial.printf("The new characteristic value as an int is: %d\n", val);
+//         drawScreenTextWithBackground(String(val) + " read from BLE characteristic", TFT_GREEN);        
+
+//     } else if (previouslyConnected) {
+//         drawScreenTextWithBackground("Disconnected. Reset M5 device to reinitialize BLE.", TFT_RED);
+//         timer = 0;
+//     }
+
+//     delay(1000);
+// }
+
+// void drawScreenTextWithBackground(String text, int backgroundColor) {
+//     M5.Lcd.fillScreen(backgroundColor);
+//     M5.Lcd.setCursor(0,0);
+//     M5.Lcd.println(text);
+// }
+
+// void broadcastBleServer() {
+//     // Start broadcasting (advertising) BLE service
+//     bleServer = BLEDevice::createServer();
+//     bleServer->setCallbacks(new myServerCallbacks());
+//     bleService = bleServer->createService(SERVICE_UUID);
+//     bLeCharacteristic = bleService->createCharacteristic(
+//         CHARACTERISTIC_UUID,
+//         BLECharacteristic::PROPERTY_READ |
+//         BLECharacteristic::PROPERTY_WRITE |
+//         BLECharacteristic::PROPERTY_NOTIFY |
+//         BLECharacteristic::PROPERTY_INDICATE 
+//     );
+//     bLeCharacteristic->setValue("Hello BLE World from Elijah");
+//     bleService->start();
+
+//     // Broadcast your bluetooth service code
+//     BLEAdvertising *bleAdvertising = BLEDevice::getAdvertising();
+//     bleAdvertising->addServiceUUID(SERVICE_UUID);
+//     bleAdvertising->setScanResponse(true);
+//     bleAdvertising->setMinPreferred(0x06); // Specifically help with iphone connection issue
+//     bleAdvertising->setMinPreferred(0x12);
+//     BLEDevice::startAdvertising();
+//     Serial.println("Characterisic defined...you can now connect with your phone!");
+// }
